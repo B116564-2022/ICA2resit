72e9caad488b59bb40f3a7e55a6d0ac4f349c26a
B116564-2022 <zebinisa.mirakbarova@gmail.com>
2023-03-04 14:24:51 +0000

    Downloaded fasta are being assigned to oqsillar var now

--- resit2.py
+++ resit2.py
@@ -1,34 +1,41 @@
-import os
+ import os
 
 #got  my AP from NCBI, should now work 
 os.environ['NCBI_API_KEY'] = 'cb6b860872dfccb5f04396d6f056d4b36c08'
 
 ###Introducing the program name, purpose and taking values from the users
+import os
+
 while True:
     print("Welcome to OQSIL-Online protein Quality aSsesIng tooL")
     oqsil = input("Please enter the name of the protein you would like to analyze: ")
     takson = input("Please enter the Taxon ID(txid): ")
-
+    print("Connecting to NCBI")
+    print(f"Searching for {oqsil} fastas for txid{takson}")
+    print("One more second")
     # sending request to ncbi for the with protein name and takson
     query = f'{oqsil}[Protein Name] AND txid{takson}[Organism]'
     search_cmd = f'esearch -db protein -query "{query}" | efetch -format fasta'
     num_results = os.popen(f'{search_cmd} | grep ">" | wc -l').read().strip()
 
-    # clarifying whether  user they wants to download all fastas
+    # clarifying whether user they wants to download all fastas
+    print("Generating results")
     print(f'Number of results found: {num_results}')
-    download_all = input(f'Do you want to download all of the {oqsil} fastas for for txid {takson} ? (y/n): ')
+    skachay_vsyo = input(f'Do you want to download all of the {oqsil} fastas for for txid {takson} ? (y/n): ')
 
-    if download_all.lower() == 'y':
+    if skachay_vsyo.lower() == 'y':
         # downloading all fastas
         print("Downloading all the fastas")
-        download_cmd = f'{search_cmd} > oqsil.fa'
-        os.system(download_cmd)
+        skachivaem = f'{search_cmd} > {oqsil}.fa'
+        os.system(skachivaem)
         print(f'All {num_results} fastas for txid {takson} have been saved to {oqsil}.fa.')
+        with open(f'{oqsil}.fa', 'r') as oqsillar:
+            fasta_tarkibi = oqsillar.read()
     else:
         # identifying whether the user wants to filter the data for specific organism
-        organism_input = input('Do you want to specify the organism? (y/n): ')
+        organism_kirit = input('Do you want to specify the organism? (y/n): ')
 
-        if organism_input.lower() == 'y':
+        if organism_kirit.lower() == 'y':
             organism = input('Enter organism name or taxid: ')
             query += f' AND {organism}[Organism]'
             search_cmd = f'esearch -db protein -query "{query}" | efetch -format fasta'
@@ -38,11 +45,14 @@ while True:
             if download_spec.lower() == 'y':
                 # downloading filteres fastas
                 print(f"Downloading {oqsil} fasta files for {organism} ")
-                download_cmd = f'{search_cmd} > {oqsil}.fa'
-                os.system(download_cmd)
+                skachivaem = f'{search_cmd} > {oqsil}.fa'
+                os.system(skachivaem)
                 print(f'All {num_results} {oqsil} fastas for {organism} have been downloaded to {oqsil}.fa.')
+                with open(f'{oqsil}.fa', 'r') as oqsillar:
+                    fasta_tarkibi = oqsillar.read()
         else:
             # does user want to search for another protein?
-            search_again = input('Would You like to continue your research on another protein? (y/n): ')
-            if search_again.lower() == 'n':
+            yana_qidir = input('Would You like to continue your research on another protein? (y/n): ')
+            if yana_qidir.lower() == 'n':
                 break
+
